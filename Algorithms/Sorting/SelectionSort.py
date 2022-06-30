9367a14e89fff1260dd06123589b7f0980603274
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-06-30 22:44:31 +0300

    Selection Sort

---
+++ Algorithms/Sorting/SelectionSort.py
@@ -0,0 +1,23 @@
+def selection_sort(arr:list) -> list :
+    for i in range(len(arr)):
+    
+        min_index = i
+        
+        for j in range(i+1,len(arr)):
+            if arr[j] < min_val:
+            
+                min_index = j
+                
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        
+
+    return arr
+
+test_cases = [
+    [6,9,5,1,0,8,20,24,14,3],
+    [1,4,6,0,4,9,2,5,9],
+    [0,2,2,2,4,5,6,9,1]
+]
+
+for case in test_cases:
+    print(selection_sort(case))
